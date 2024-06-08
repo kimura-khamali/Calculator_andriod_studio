38da07b56496ec00c0d0e8e31a21f34e5c4af533
kimura-khamali <kimurakhamali@gmail.com>
2024-06-08 09:36:51 +0300

    First andriod App

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -30,48 +30,36 @@ class MainActivity : AppCompatActivity() {
         val num1Str = binding.etnum1.text.toString()
         val num2Str = binding.etnum2.text.toString()
 
-        // Check if num1 and num2 are integers or decimals
-        val num1IsDecimal = num1Str.contains(".")
-        val num2IsDecimal = num2Str.contains(".")
+
 
         var inputError = false
         if (num1Str.isBlank()) {
             inputError = true
             binding.etnum1.error = "Num 1 is required"
-        } else if ((num1IsDecimal && !num2IsDecimal) || (!num1IsDecimal && num2IsDecimal)) {
-            inputError = true
-            binding.etnum1.error = "Both numbers should be either integers or decimals"
         }
+
         if (num2Str.isBlank()) {
             inputError = true
             binding.etnum2.error = "Num 2 is required"
-        } else if ((num1IsDecimal && !num2IsDecimal) || (!num1IsDecimal && num2IsDecimal)) {
-            inputError = true
-            binding.etnum2.error = "Both numbers should be either integers or decimals"
         }
+
 
         if (!inputError) {
             // Convert to Double since we want to support both integers and decimals
             calculate(num1Str.toDouble(), num2Str.toDouble(), sign)
         }
     }
-
-    fun calculate(num1: Double, num2: Double, sign: String) {
+    fun calculate(num1:Double,num2:Double,sign:String){
+//        val num1 = etnum1.text.toString().toInt()
+//        val num2 = etnum2.text.toString().toDouble()
         var result = 0.0
 
-        when (sign) {
-            "+" -> result = num1 + num2
-            "-" -> result = num1 - num2
-            "*" -> result = num1 * num2
-            "/" -> {
-                if (num2 != 0.0) {
-                    result = num1 / num2
-                } else {
-//                    binding.tvResult.text = "Error: Division by zero"
-                    return
-                }
-            }
+        when(sign){
+            "+"->result= num1+num2
+            "-"->result= num1-num2
+            "*"->result= num1*num2
+            "/"->result= num1/num2
         }
-        binding.tvResult.text = result.toString()
+binding.tvResult.text = result.toString()
     }
 }
